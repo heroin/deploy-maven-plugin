f8f8133175603956e003ac41195097df8577167e
Heroin <heroin.nee@gmail.com>
2013-01-06 22:58:43 +0800

    first

---
+++ src/main/java/so/heroin/maven/plugins/deploy/AbstractDeployMojo.java
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package so.heroin.maven.plugins.deploy;
+
+import org.apache.maven.plugin.AbstractMojo;
+
+import java.io.File;
+
+public abstract class AbstractDeployMojo extends AbstractMojo {
+
+    protected String path;
+    protected String username;
+    protected String password;
+    protected String ip;
+    protected File cert;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public File getCert() {
+        return cert;
+    }
+
+    public void setCert(File cert) {
+        this.cert = cert;
+    }
+}
